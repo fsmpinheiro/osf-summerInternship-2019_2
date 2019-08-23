c57734a0824118badf4064c5a09a6e682307790d
Samuel Pinheiro <31665870+fsmpinheiro@users.noreply.github.com>
2019-08-23 19:23:27 -0300

    top-header
    starting over...

---
+++ src/components/Header.vue
@@ -0,0 +1,141 @@
+<template>
+    <header id="top-header" class="main-header">
+        <div class="wrapper">
+      <nav class="container-header">
+        <div id="item-1-logoBrand" class="c-OSFbrand">
+          <ul>
+            <a href="#">
+              <li>
+                <div>
+                  <img src="../assets/imgs/ui-kit_osf-logo.png" id="osf-logo" alt="Osf Logo" class />
+                </div>
+              </li>
+              <li>
+                <div class="osf-brand">
+                  <p id="osf-brand-0">Osf</p>
+                  <p id="osf-brand-1">Academy</p>
+                </div>
+              </li>
+            </a>
+          </ul>
+        </div>
+
+        <div id="item-2-mainMenu" class="c-mainmenu">
+          <ul>
+            <li class="menu-item">
+              <a href="#">
+                Services
+                <div>
+                  <svg class="arrow-solid">
+                    <use xlink:href="#solid-arrow"/>
+                  </svg>
+                </div>
+              </a>
+            </li>
+
+            <li class="menu-item">
+              <a href="#">Company</a>
+            </li>
+
+            <li class="menu-item">
+              <a href="#">Library</a>
+            </li>
+
+            <li class="menu-item">
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+
+        <div id="item-3-nationality" class="c-nationality">
+          <ul>
+            <li>
+              <a href="#" class="set-national" id="set-national">
+                En
+                <div>
+                  <svg class="arrow-solid">
+                    <use xlink:href="#solid-arrow"/>
+                  </svg>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" class="set-currency" id="set-currency">
+                $ Us
+                <div>
+                  <svg class="arrow-solid">
+                    <use xlink:href="#solid-arrow"/>
+                  </svg>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div id="item-4-icons" class="c-icons">
+          <ul>
+            <li class>
+              <div>
+                <a href="#">
+                  <div class="panel-icon">
+                    <svg class="icon-header">
+                      <use xlink:href="#icon-search" />
+                    </svg>
+                    <!-- <svg class="icon-header icon-search" /> -->
+                    <!-- <img src="imgs/icon-search.svg" alt="panel icon seach"> -->
+                  </div>
+                </a>
+              </div>
+            </li>
+            <li class>
+              <div>
+                <a href="#">
+                  <div class="panel-icon">
+                    <svg class="icon-header">
+                      <use xlink:href="#icon-profile" />
+                    </svg>
+                    <!-- <svg class="icon-header icon-profile" /> -->
+                    <!-- <img src="imgs/icon-profile.svg" alt="panel icon profile"> -->
+                  </div>
+                </a>
+              </div>
+            </li>
+            <li class>
+              <div>
+                <a href="#">
+                  <div class="panel-icon">
+                    <svg class="icon-header">
+                      <use xlink:href="#icon-favorites" />
+                    </svg>
+                    <!-- <svg class="icon-header icon-favorites" /> -->
+                    <!-- <img src="imgs/icon-favorites.svg" alt="panel icon heart"> -->
+                  </div>
+                </a>
+              </div>
+            </li>
+            <li class>
+              <div>
+                <a href="#">
+                  <div class="panel-icon">
+                    <svg class="icon-header">
+                      <use xlink:href="#icon-shopping-bag" />
+                    </svg>
+                    <!-- <svg class="icon-header icon-shopping-bag" /> -->
+                    <!-- <img src="imgs/icon-shopping-bag.svg" alt="panel icon bag"> -->
+                  </div>
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    </header>
+</template>
+
+
+
+<style scoped>
+
+</style>
